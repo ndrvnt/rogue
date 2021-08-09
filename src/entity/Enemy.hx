ee0125a494b4b4d3547cfeaf9ca0e3405875553e
Andrea <andrea.venturi@gmail.com>
2021-08-09 23:36:01 +0200

    Entita mostro

---
+++ src/entity/Enemy.hx
@@ -0,0 +1,22 @@
+package entity;
+
+import h2d.col.Point;
+
+class Enemy extends Entity {
+	var hp:Int;
+
+	override public function new(tipo:Int, pos:Point) {
+		super();
+		this.x = pos.x * 32;
+		this.y = pos.y * 32;
+		this.tipo = tipo;
+		this.sprite = this.getSprite();
+		this.addChild(this.sprite);
+	}
+
+	private function getSprite():h2d.Bitmap {
+		var sprite:h2d.Bitmap = new h2d.Bitmap(hxd.Res.demonspawn_red_m.toTile());
+		this.hp = 20;
+		return sprite;
+	}
+}
