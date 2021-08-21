35660781bb04433da0afdfcd94df83c5216c3b38
Andrea <andrea.venturi@gmail.com>
2021-08-21 10:17:33 +0200

    integrato con inventario

--- src/mappa/Inventario.hx
+++ src/mappa/Inventario.hx
@@ -1,38 +1,93 @@
 package mappa;
 
 import entity.Oggetto;
-import entity.Zaino;
 
+@:publicFields
 class Inventario extends h2d.Object {
 	var inventario:Array<Oggetto>;
 	var font:h2d.Font;
+	var pos:Int = 0;
+	var gg:Game;
 
 	public static final instance:Inventario = new Inventario();
 
 	private function new() {
 		super();
+
 		// this.font = hxd.res.DefaultFont.get();
-		this.inventario = Zaino.instance.inventario;
+		this.inventario = new Array<Oggetto>();
+		// hxd.Window.getInstance().addEventTarget(this.cambiaPos);
+	}
+
+	public function aggiungi(ogg:Oggetto) {
+		this.inventario.push(ogg);
+	}
+
+	public function attiva() {
+		trace("Attivo inventario");
+		this.gg = Game.instance;
+		hxd.Window.getInstance().addEventTarget(this.cambiaPos);
+	}
+
+	public function disattiva() {
+		trace("disattivo inventario");
+		this.gg = null;
+		hxd.Window.getInstance().removeEventTarget(this.cambiaPos);
+	}
+
+	public function cambiaPos(event:hxd.Event) {
+		switch (event.kind) {
+			case EKeyDown:
+				if (event.keyCode == 83) {
+					this.pos = (this.pos + 1) % this.inventario.length;
+				}
+				if (event.keyCode == 87) {
+					this.pos = (this.pos - 1);
+					if (this.pos < 0)
+						this.pos += this.inventario.length;
+				}
+				if (event.keyCode == 73) {
+					this.gg.mostraMappa();
+					return;
+				}
+			case _:
+		}
+		this.aggiorna();
 	}
 
 	public function aggiorna() {
 		this.removeChildren();
-		for (ind in 0...this.inventario.length) {
-			this.inventario[ind].x = 0;
-			this.inventario[ind].y = ind * 64;
 
-			this.inventario[ind].scaleX = 2;
-			this.inventario[ind].scaleY = 2;
+		if (this.inventario.length == 0) {
+			this.pos = 0;
+		} else {
+			for (ind in 0...this.inventario.length) {
+				if (ind == this.pos) {
+					var base:Int = (ind * 64) + 16;
+					var g = new h2d.Graphics(this);
+					g.lineStyle(2, 0xFF0000);
+					g.moveTo(1, base);
+					g.lineTo(30, base + 16);
+					g.lineTo(1, base + 32);
+					g.lineTo(1, base);
+				}
 
-			var tf = new h2d.Text(hxd.res.DefaultFont.get());
-			tf.x = 68;
-			tf.y = ind * 64;
-			tf.scaleX = 2;
-			tf.scaleY = 2;
-			tf.text = "Pozione";
+				this.inventario[ind].x = 20;
+				this.inventario[ind].y = ind * 64;
 
-			this.addChild(this.inventario[ind]);
-			this.addChild(tf);
+				this.inventario[ind].scaleX = 2;
+				this.inventario[ind].scaleY = 2;
+
+				var tf = new h2d.Text(hxd.res.DefaultFont.get());
+				tf.x = 68;
+				tf.y = ind * 64;
+				tf.scaleX = 2;
+				tf.scaleY = 2;
+				tf.text = "Pozione";
+
+				this.addChild(this.inventario[ind]);
+				this.addChild(tf);
+			}
 		}
 	}
 }
