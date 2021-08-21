539bb551dc909fbc88c4a207186498c0c0a25bc4
Andrea <andrea.venturi@gmail.com>
2021-08-21 10:17:53 +0200

    timer su cambio scena per non imppallare il sistema con m,ultiple richieste

--- src/Game.hx
+++ src/Game.hx
@@ -7,26 +7,46 @@ class Game extends hxd.App {
 	var inventario:Inventario;
 	var scenaMappa:h2d.Scene;
 	var scenaInventario:h2d.Scene;
+	var cambio:Bool = true;
 
 	override function init() {
 		super.init();
 		this.scenaMappa = new h2d.Scene(); // create a new scene
-		setScene(this.scenaMappa); // set it as the current scene
-		this.mappa = Mappa.instance;
-		scenaMappa.addChild(this.mappa);
-		this.mappa.generaTabella();
 		this.scenaInventario = new h2d.Scene();
+		this.mappa = Mappa.instance;
 		this.inventario = Inventario.instance;
 		this.scenaInventario.addChild(this.inventario);
+		scenaMappa.addChild(this.mappa);
+		this.mappa.generaTabella();
+		setScene(this.scenaMappa); // set it as the current scene
 	}
 
 	public function mostraInventario() {
-		this.inventario.aggiorna();
-		setScene(this.scenaInventario);
+		if (this.cambio) {
+			this.cambio = false;
+			this.mappa.hero.disattiva();
+			this.inventario.attiva();
+			this.inventario.aggiorna();
+			setScene(this.scenaInventario);
+			var timer = new haxe.Timer(500);
+			timer.run = function() {
+				this.cambio = true;
+			}
+		}
+		// setScene(this.scenaInventario);
 	}
 
 	public function mostraMappa() {
-		setScene(this.scenaMappa);
+		if (this.cambio) {
+			this.cambio = false;
+			this.mappa.hero.attiva();
+			this.inventario.disattiva();
+			setScene(this.scenaMappa);
+			var timer = new haxe.Timer(500);
+			timer.run = function() {
+				this.cambio = true;
+			}
+		}
 	}
 
 	override function update(dt:Float):Void {}
